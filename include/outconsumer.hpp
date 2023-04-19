71cde914611eb972c7a00b5176f5114208586321
EddyGlezz <eddyglezz95@gmail.com>
2023-04-19 12:15:25 +0200

    init repo

---
+++ include/outconsumer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include<QObject>
+#include<QString>
+#include<QByteArray>
+#include<QJsonArray>
+#include<QtQml/qqmlregistration.h>
+#include"nodeConnection.hpp"
+#include"account.hpp"
+
+
+using namespace qiota::qblocks;
+using namespace qiota;
+
+class OutConsumer : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+public:
+    OutConsumer(QObject *parent = nullptr):QObject(parent){};
+
+    Q_INVOKABLE void consume(std::vector<qiota::Node_output>  outs, QJsonObject address, QVector<quint32> subpath={0,0,0},QString amountneedit="0");
+
+
+signals:
+    void finish(QJsonObject addreses, qiota::AddressBundle bundle);
+
+
+private:
+
+
+};
---
+++ src/outconsumer.cpp
@@ -0,0 +1,40 @@
+#include"outconsumer.hpp"
+#include"encoding/qbech32.hpp"
+#include"QJsonDocument"
+#include"QJsonObject"
+
+using namespace qiota;
+
+void OutConsumer::consume(std::vector<qiota::Node_output>  outs, QJsonObject address, QVector<quint32> subpath,QString amountneedit)
+{
+    const quint64 amount_need_it=amountneedit.toULongLong();
+    const auto addrptr=qblocks::Address::from_(address);
+
+    auto bundle= (addrptr->type()==qblocks::Address::Ed25519_typ)?Account::get_addr(subpath):AddressBundle(addrptr);
+
+    bundle.consume_outputs(outs,amount_need_it);
+
+    QJsonArray aliasad;
+    for(const auto& v: bundle.alias_outputs)
+    {
+        aliasad.push_back(std::shared_ptr<qblocks::Address>(new Alias_Address(v->get_id()))->get_Json());
+    }
+
+    QJsonArray nftad;
+    for(const auto& v: bundle.nft_outputs)
+    {
+        nftad.push_back(std::shared_ptr<qblocks::Address>(new NFT_Address(v->get_id()))->get_Json());
+    }
+
+    QJsonArray foundryid;
+    for(const auto& v: bundle.foundry_outputs)
+    {
+        foundryid.push_back(v->get_id().toHexString());
+    }
+    QJsonObject var;
+    var.insert("alias",aliasad);
+    var.insert("nft",nftad);
+    var.insert("foundry",foundryid);
+
+    emit finish(var,bundle);
+}
